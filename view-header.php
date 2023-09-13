deef74e36417c44e559b598cc117dbde018552a4
gjwilliams716 <gjwilliams716@gmail.com>
2023-09-13 13:47:22 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,33 +1,40 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  </head>
-  <body style="margin:auto">
-    <h1 style="margin-left:10px">MIS 4013 HW 2: Homepage</h1>
- 
-    <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="index.html">Homepage</a> <!--add link to other pages in href, last li is disabled -->
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="post-result">Post Result</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="get.php">Get</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="get-result.php">Get Result</a>
-          </li>
-      </ul> 
-  </body>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <?php
-include "view-header.php";
-?>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+      <a class="nav-link active" aria-current="page" href="/">Home</a>
+  </li>
+      <li class="nav-item">
+        <a class="nav-link" href="post.php">Post</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="post-result.php">Post Result</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="get.php">Get</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="get-result.php">Get Result</a>
+      </li>
+    </ul>
+  </div>
+</nav>
