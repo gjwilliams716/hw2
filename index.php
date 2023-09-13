8699af8bad5a173bab6b9017ce7744b787e69347
gjwilliams716 <gjwilliams716@gmail.com>
2023-09-13 13:37:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,47 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
   </head>
-  <body style="margin:auto">
-    <h1 style="margin-left:10px">MIS 4013 HW 2: Homepage</h1>
- 
-    <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="index.html">Homepage</a> <!--add link to other pages in href, last li is disabled -->
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="post-result">Post Result</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="get.php">Get</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="get-result.php">Get Result</a>
-          </li>
-      </ul> 
+  <body>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+      <a class="nav-link active" aria-current="page" href="/">Home</a>
+  </li>
+      <li class="nav-item">
+        <a class="nav-link" href="post.php">Post</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="post-result.php">Post Result</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="get.php">Get</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="get-result.php">Get Result</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <h1>Hello, world!</h1>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
-
-  <?php
-$pageTitle = "Home"
-include "view-header.php";
-?>
-
-  <h1>Home</h1>
-
-  <?php
-include "view-footer.php";
-?>
+</html>
